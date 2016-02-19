80edac36cb18673e81a73e7396126abf952c49ec
antrax <antrax@diego-Notebook-PC>
2016-02-18 21:01:46 -0500

    se agrego un echo con el mensaje hola mundo dentro de etiquetas <>h1

--- CRUD/index.php
+++ CRUD/index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+                echo '<h1>HOLA MUNDO</h1>';
         ?>
     </body>
 </html>
