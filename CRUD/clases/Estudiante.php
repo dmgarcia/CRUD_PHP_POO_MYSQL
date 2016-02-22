a191946d7eb25b18299c56cbef1fe3f508616389
antrax <antrax@diego-Notebook-PC>
2016-02-21 21:16:46 -0500

    Creaci

---
+++ CRUD/clases/Estudiante.php
@@ -0,0 +1,24 @@
+<?php
+
+    //CLASE DE CONEXION INCLUIDA
+    include_once './Conexion.php';
+    
+    class Estudiante{
+        
+        //Atributos
+        private $id;
+        private $cedula;
+        private $nombre;
+        private $apellido;
+        private $telefono;
+        private $edad;
+        private $promedio;
+        private $fecha;
+        private $con;
+        
+        
+        //Metodos
+    }
+
+?>
+
